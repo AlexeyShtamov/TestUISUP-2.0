40dd9bf0aa0c66482252625e496b126215b9144c
Alexey <alexshamox@gmail.com>
2025-04-12 20:21:59 +0500

    small changes

--- src/main/java/ru/shtamov/uisupTest/domain/Module.java
+++ src/main/java/ru/shtamov/uisupTest/domain/Module.java
@@ -8,10 +8,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-/**
- * Модуль образовательной программы.
- * Модуль может принадлежать нескольким ОП
- */
 @Entity
 @Builder
 @AllArgsConstructor
@@ -20,22 +16,15 @@ import java.util.UUID;
 @Setter
 public class Module {
 
-    /** Итендификатор */
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID uuid;
 
-    /** Название */
     private String title;
 
-    /**
-     * Тип модуля
-     * @see ModuleType
-     */
     @Enumerated(EnumType.STRING)
     private ModuleType moduleType;
 
-    /** Список ОП, в которые входит данный модуль*/
     @ManyToMany(mappedBy = "modules")
     private List<EducationProgram> educationPrograms = new ArrayList<>();
 }
